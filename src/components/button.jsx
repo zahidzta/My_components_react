39a9fdcf6dd07f7c4a5c93590420bace3fd42738
zetta <zahidduron@gamail.com>
2024-08-10 18:47:18 -0600

    Se modifican los colores de hover de los botones solidos y se cambia el timpo de transición

--- src/components/button.jsx
+++ src/components/button.jsx
@@ -1,14 +1,16 @@
 import React, { useState } from 'react';
 
+//Quitar txtHover de los botones solidos y bgColor de los botones outline
+
 const variants = [
-    {variant: "primary", bgColor: "#1d35ad", txtColor: "#fff", bgHover: "#1a2d8f", txtHover: "#fff"},
-    {variant: "secondary", bgColor: "#46464a", txtColor: "#fff", bgHover: "#38383b", txtHover: "#fff"},
-    {variant: "success", bgColor: "#3a802b", txtColor: "#fff", bgHover: "#347327", txtHover: "#fff"},
-    {variant: "warning", bgColor: "#f2ef07", txtColor: "#000", bgHover: "#e6e315", txtHover: "#000"},
-    {variant: "danger", bgColor: "#bd1111", txtColor: "#fff", bgHover: "#a60f0f", txtHover: "#fff"},
-    {variant: "info", bgColor: "#4099e3", txtColor: "#000", bgHover: "#418dcc", txtHover: "#000"},
-    {variant: "light", bgColor: "#fff", txtColor: "#000", bgHover: "#e3e3e3", txtHover: "#000"},
-    {variant: "dark", bgColor: "#19191a", txtColor: "#fff", bgHover: "#2a2a2b", txtHover: "#fff"},
+    {variant: "primary", bgColor: "#1d35ad", txtColor: "#fff", bgHover: "#172985", txtHover: "#fff"},
+    {variant: "secondary", bgColor: "#46464a", txtColor: "#fff", bgHover: "#272729", txtHover: "#fff"},
+    {variant: "success", bgColor: "#3a802b", txtColor: "#fff", bgHover: "#2c6121", txtHover: "#fff"},
+    {variant: "warning", bgColor: "#f2ef07", txtColor: "#000", bgHover: "#d9d61a", txtHover: "#000"},
+    {variant: "danger", bgColor: "#bd1111", txtColor: "#fff", bgHover: "#910c0c", txtHover: "#fff"},
+    {variant: "info", bgColor: "#4099e3", txtColor: "#000", bgHover: "#347ebf", txtHover: "#000"},
+    {variant: "light", bgColor: "#fff", txtColor: "#000", bgHover: "#cfcfcf", txtHover: "#000"},
+    {variant: "dark", bgColor: "#19191a", txtColor: "#fff", bgHover: "#3c3c3d", txtHover: "#fff"},
     {variant: "outline-primary", bgColor: "#fff0", borderStyle: "1px solid #1d35ad", txtColor: "#1d35ad", bgHover: "#1d35ad", txtHover: "#fff"},
     {variant: "outline-secondary", bgColor: "#fff0", borderStyle: "1px solid #46464a", txtColor: "#46464a", bgHover: "#46464a", txtHover: "#fff"},
     {variant: "outline-success", bgColor: "#fff0", borderStyle: "1px solid #3a802b", txtColor: "#3a802b", bgHover: "#3a802b", txtHover: "#fff"},
@@ -43,7 +45,7 @@ function Button({text="Button", variant="primary", bgColor="#1d35ad", txtColor="
         borderRadius: "5px",
         cursor: "pointer",
         width: size === "lg" ? "100%" : "auto",
-        transition: "background-color 0.6s ease",
+        transition: "background-color 0.5s, color 0.5s",
     }
 
     const hoverStyle = {
